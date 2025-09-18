c1dcec864eb496c074872a104c8bf5ffc533fadc
ab1538 <ab1538@g.rit.edu>
2025-09-18 12:42:09 +0200

    COMMITING

--- characters.py
+++ characters.py
@@ -1,7 +1,49 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: ab1538
+@ASSESSME.AUTHOR: Andrej Biljaka
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+
+
+def add_char(c1,c2):
+    char1 = ord(c1)
+    char2 = ord(c2)
+    sum = char1 + char2
+    print("Sum of a and b: ",str(sum), "\nCharacter: ",chr(sum), "\n")
+
+
+def subtract_char(c3,c4):
+    char3 = ord(c3)
+    char4 = ord(c4)
+    sub = char3 - char4
+    print("Subtraction of ", c3," and ",c4," = ",str(sub),"\nCharacter: ", chr(sub), "\n")
+
+
+
+def main():
+    inp_1 = input("Enter the first character: ")
+    inp_2 = input("Enter second character: ")
+    """
+    When i run the command in terminal it asks for an input and after i enter the input it shows the sum and difference 
+    After i saw that it doesnt show the characters for difference
+    i googled it and it showed that when the difference is negative it crashes 
+    Moreover the numebrs has to be between 33 and 127 in order to ascii table to be able to output visible output
+    in other cases are the commands which can not be visible
+    """
+
+    add_char(inp_1,inp_2)
+    subtract_char(inp_1,inp_2)
+
+
+"""
+When 2 or more characters are entered code crashesh because there are only 2 parametrs which can be used in the funciton
+Yes i understand the way it works because i see the output and uderstand the error 
+
+
+"""
+
+
+if __name__ == "__main__":
+    main()
 
--- scope.py
+++ scope.py
@@ -1,6 +1,42 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: ab1538
+@ASSESSME.AUTHOR: Andrej Biljaka
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+
+STRING_GLOBAL = "Hello Everyone!"
+INT_GLOBAL = 55
+FLOAT_GLOBAL = 6.90
+
+
+def print_param(param):
+    print("print_param:", param)
+
+
+def print_local():
+    local_var = "I am a local variable"
+    print("print_local:", local_var)
+
+
+def print_which():
+    STRING_GLOBAL = 999
+    print("print_which:", STRING_GLOBAL)
+
+
+def main():
+    print_param(STRING_GLOBAL)
+    print_param(INT_GLOBAL)
+    print_param(FLOAT_GLOBAL)
+
+    local_var = "I am another local variable"
+    print("main local_var:",local_var)
+
+    print_local()
+
+    print_which()
+    print("Global STRING_GLOBAL:", STRING_GLOBAL)
+
+
+if __name__ == "__main__":
+    main()
